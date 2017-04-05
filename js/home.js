a116e96803e88394958835ba302833dba38cbaba
Jacqueline <Jacqueline.MaryK@gmail.com>
2017-04-05 09:49:18 -0600

    changed the nav bar to a ul li

--- js/home.js
+++ js/home.js
@@ -1,3 +1,20 @@
+$(document).ready(function () {
+  $("li").click(function () {
+    $('li > ul').not($(this).children("ul").toggle()).hide();
+  });
+});
+
+$(document).on('click',function (e) {
+  footerUl = $('.top-ul:first li');
+  if (!footerUl.is(e.target) 
+      && footerUl.has(e.target).length === 0){
+    footerUl.children('ul').hide();
+  }
+});
+
+
+
+
 $(".project-scroll").click(function() {
     $('html, body').animate({
         scrollTop: $("#projects-top").offset().top
